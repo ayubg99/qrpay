81ff78e2e6bab5dbb2ae57fded7c5a31498f2d74
Ayoub Gharbi <ayoubg9919@gmail.com>
2023-05-11 12:22:55 +0200

    add bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,15 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
+
+  $(document).ready(function() {
+    $('.navbar-toggler').click(function() {
+      $('.navbar-collapse').toggleClass('show');
+    });
+  });
+
+
