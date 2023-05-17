84d005363a6e7735ae142c1781f0a63e4d7b147e
Ayoub Gharbi <ayoubg9919@gmail.com>
2023-05-17 22:35:32 +0200

    change price field type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_16_200601) do
+ActiveRecord::Schema.define(version: 2023_05_17_170636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2023_05_16_200601) do
   create_table "food_items", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.integer "price"
+    t.decimal "price", precision: 8, scale: 2
     t.bigint "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
