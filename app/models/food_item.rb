0d0df8d90b3a04eefc06f6239ed23e2d56ae5367
Ayoub Gharbi <ayoubg9919@gmail.com>
2023-07-19 17:54:46 +0200

    create deleted order cart items

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -5,10 +5,6 @@ class FoodItem < ApplicationRecord
   has_many :food_types, through: :food_type_food_items
   has_many :cart_item_food_items, dependent: :destroy
   has_many :cart_items, through: :cart_item_food_items
-  has_many :order_food_items, dependent: :destroy
-  has_many :orders, through: :order_food_items
-  has_many :deleted_order_food_items, dependent: :destroy
-  has_many :deleted_orders, through: :deleted_order_food_items
   has_many :special_menu_food_items, dependent: :destroy
   
   has_one_attached :image
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,9 +2,9 @@ class Order < ApplicationRecord
   belongs_to :restaurant
   has_many :cart_items, dependent: :nullify
 
-  before_destroy :create_order_history
+  after_create :create_order_history
 
-  	def add_cart_items_from_cart(cart)
+  def add_cart_items_from_cart(cart)
 		cart.cart_items.each do |item|
 			item.cart_id = nil
 			cart_items << item
@@ -12,14 +12,14 @@ class Order < ApplicationRecord
 	end
 
 	def create_order_history
-        deleted_order = DeletedOrder.create( 
+        deleted_order = DeletedOrder.new( 
           restaurant_id: restaurant_id,
           total_price: total_price,
           order_date: created_at
         )
 
-        cart_items.each do |cart_item| 
-          deleted_order.cart_items << cart_item
+        cart_items.each do |cart_item|
+			    deleted_order.cart_items << cart_item
         end
 
         deleted_order.save
