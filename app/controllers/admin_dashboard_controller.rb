2e67011624885b11452d08df276f2ae56ce3946e
Ayoub Gharbi <ayoubg9919@gmail.com>
2023-07-21 22:58:25 +0200

    add restaurant location details

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -3,5 +3,14 @@ class AdminDashboardController < ApplicationController
     
     def index
         @restaurants = Restaurant.all
+        @current_day_revenue = DailyRevenue.where(day: Date.today.day, month: Date.today.month, year: Date.today.year).sum(:revenue) * 0.02
+        @current_month_revenue = MonthlyRevenue.where(month: Date.today.month, year: Date.today.year).sum(:revenue) * 0.02
+        @current_year_revenue = MonthlyRevenue.where(year: Date.today.year).sum(:revenue) * 0.02
+        @revenues = DailyRevenue.where(month: Date.today.month, year: Date.today.year)
+                            .pluck(:date, :revenue)
+    end
+
+    def restaurants
+        @restaurants = Restaurant.all
     end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   }
 
   get '/admin_dashboard', to: 'admin_dashboard#index', as: 'admin_dashboard'
+  get '/admin_dashboard_restaurants', to: 'admin_dashboard#restaurants', as: 'admin_dashboard_restaurants'
   resources :restaurants do 
     get '/dashboard', to: 'dashboard#index', as: 'dashboard'
     get '/dashboard/orders', to: 'dashboard#orders', as: 'dashboard_orders'
