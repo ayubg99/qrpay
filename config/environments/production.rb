e93fc488da35cb84782b78f44775a56fb7e7787e
Ayoub Gharbi <ayoubg9919@gmail.com>
2023-07-30 23:38:46 +0200

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.js_compressor = Uglifier.new(harmony: true) if defined? Uglifier
 end
