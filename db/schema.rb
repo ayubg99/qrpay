8dee004752df43a4065b90f0e91efb0c519f1679
Ayoub Gharbi <ayoubg9919@gmail.com>
2023-08-05 17:39:41 +0200

    add table params to links

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_22_131801) do
+ActiveRecord::Schema.define(version: 2023_08_05_145147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -233,6 +233,7 @@ ActiveRecord::Schema.define(version: 2023_07_22_131801) do
     t.datetime "updated_at", null: false
     t.string "payment_method"
     t.decimal "total_price", precision: 8, scale: 2
+    t.integer "table_number"
     t.index ["cart_id"], name: "index_orders_on_cart_id"
     t.index ["restaurant_id"], name: "index_orders_on_restaurant_id"
   end
