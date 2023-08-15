f5e9ef90c5cef19b2e93fe415f72b22ab9c51d29
Ayoub Gharbi <ayoubg9919@gmail.com>
2023-08-15 19:00:06 +0200

    change font

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,8 @@ class Category < ApplicationRecord
   belongs_to :restaurant
   has_many :food_items, dependent: :destroy
 
+  before_save :name_uppercase
+
   validates :name, presence: true
 
   def name_uppercase 
